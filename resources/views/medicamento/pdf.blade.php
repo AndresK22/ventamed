97918841a5735374a527e9e1d8b1ff6daf4f96b8
AndresK22 <hg19010@ues.edu.sv>
2023-01-24 15:00:46 -0600

    Arreglo de rutas pdf

--- resources/views/medicamento/pdf.blade.php
+++ resources/views/medicamento/pdf.blade.php
@@ -11,12 +11,10 @@
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="{{ public_path('css/materialize.min.css') }}"  media="screen"/>
-    <link type="text/css" rel="stylesheet" href="{{ public_path('css/app.css') }}"  media="screen"/>   
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/materialize.min.css') }}"  media="screen"/>
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/app.css') }}"  media="screen"/>   
 
-    <script type="text/javascript" src="{{ public_path('js/materialize.min.js') }}"></script>
-    <script type="text/javascript" src="{{ public_path('js/app.js') }}"></script>
-    <script type="text/javascript" src="{{ public_path('js/jquery-3.2.1.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/materialize.min.js') }}"></script>
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
